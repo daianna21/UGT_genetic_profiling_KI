c0fc68f4dec40bd8dba3e3d3c2159f2c1138c6af
daianna21 <daianna2121@outlook.es>
2023-10-25 19:57:25 +0200

    Add pop scale analysis script

--- code/04_Population_scale_analysis.R
+++ code/04_Population_scale_analysis.R
@@ -1,4 +1,35 @@
 
+library(here)
+library(ggplot2)
+library(sessioninfo)
+
+
+####################################################################################################
+##                    4. Population-scale analysis of deleterious UGT variants
+####################################################################################################
+
+gene_families <- c('UGT1', 'UGT2', 'UGT3', 'UGT8')
+
+UGT1_genes <- c('UGT1A1', 'UGT1A3', 'UGT1A4', 'UGT1A5', 'UGT1A6', 'UGT1A7', 'UGT1A8', 'UGT1A9', 'UGT1A10')
+UGT2_genes <- c('UGT2A1', 'UGT2A2', 'UGT2A3', 'UGT2B4', 'UGT2B7', 'UGT2B10', 'UGT2B11', 'UGT2B15', 'UGT2B17', 'UGT2B28')
+UGT3_genes <- c('UGT3A1', 'UGT3A2')
+UGT8_genes <- c('UGT8')
+
+UGT_genes <- c('UGT1A1', 'UGT1A3', 'UGT1A4', 'UGT1A5', 'UGT1A6', 'UGT1A7', 'UGT1A8', 'UGT1A9', 'UGT1A10',
+               'UGT2A1', 'UGT2A2', 'UGT2A3', 'UGT2B4', 'UGT2B7', 'UGT2B10', 'UGT2B11', 'UGT2B15', 'UGT2B17', 'UGT2B28',
+               'UGT3A1', 'UGT3A2', 
+               'UGT8')
+
+## Load exonic vars per gene
+for (gene in UGT_genes){
+  exonic_vars <- eval(parse_expr(load(here(paste0('~/Desktop/UGT_genetic_profiling_KI/processed-data/03_Anno_functional_impact/', 
+                                                  gene, '_exonic_data.Rdata')) )))
+  assign(paste0(gene, '_exonic_data'), exonic_vars)
+}
+## Load Variant - Gene - GMAF info
+load(here('processed-data/03_Anno_functional_impact/GMAFs_Dvars_genes.Rdata'))
+## Add gene family info
+GMAFs_Dvars_genes$gene_fam <- substring(GMAFs_Dvars_genes$gene, 1, 4)
 
 ## Plot allele frequencies of all D variants within each population
 
@@ -8,8 +39,7 @@ populations <- c('African_or_African_American',
                  'East_Asian',
                  'European_Finnish',
                  'European_non_Finnish',
-                 'South_Asian',
-                 'Other')
+                 'South_Asian')
 
 allD_vars_MAF_in_pops <- vector()
 for (gene in UGT_genes){
@@ -17,14 +47,13 @@ for (gene in UGT_genes){
   ## Exonic variants per gene
   exonic_data <- eval(parse_expr(paste0(gene, '_exonic_data')))
   
-  ## Compute MAF within each population 
+  ## Compute MAF of variants within each population 
   for (p in populations){
     exonic_data[,paste0('MAF_', p)] <- exonic_data[,paste0('Allele_Count_', p)]/exonic_data[,paste0('Allele_Number_', p)]
     ## If no MAF available in the population
-    nan <- which(is.nan(exonic_data[,paste0('MAF_', p)]))
-    exonic_data[nan, paste0('MAF_', p)] <- NA
+    replace(exonic_data[,paste0('MAF_', p)], which(is.nan(exonic_data[,paste0('MAF_', p)])), NA)
   }
-  ## MAF of D vars in each population
+  ## Subset to D vars 
   data <- exonic_data[which(exonic_data$Functional_impact=='D'),c('Variant_ID', 'Allele_Frequency', paste0('MAF_', populations))]
   data <- melt(data, id.vars = c('Variant_ID'))
   colnames(data) <- c('Variant_ID', 'Group', 'MAF')
@@ -37,16 +66,20 @@ allD_vars_MAF_in_pops$Group <- factor(allD_vars_MAF_in_pops$Group, levels=c(past
 
 ## Number of variants with missing MAF per population
 table(allD_vars_MAF_in_pops[which(!is.na(allD_vars_MAF_in_pops$MAF)), 'Group'])
-# Allele_Frequency MAF_African_or_African_American  MAF_Latino_or_Admixed_American            MAF_Ashkenazi_Jewish 
-#             1206                            1206                            1206                            1206 
-# MAF_East_Asian            MAF_European_Finnish        MAF_European_non_Finnish                 MAF_South_Asian 
-#           1206                            1206                            1206                            1131 
-# MAF_Other 
-#      1206 
+# MAF_African_or_African_American  MAF_Latino_or_Admixed_American            MAF_Ashkenazi_Jewish                  MAF_East_Asian 
+#                            1206                            1206                            1206                            1206 
+# MAF_European_Finnish        MAF_European_non_Finnish                 MAF_South_Asian                Allele_Frequency 
+#                 1206                            1206                            1131                            1206 
+
+## Manually add MAF of 2−234668879−C−CAT (UGT1A1) variant in South Asians
+
+
+
 
 ## Label variants with MAF>0.01; ignore missing MAFs
 allD_vars_MAF_in_pops$Label <- apply(allD_vars_MAF_in_pops, 1, function(x){if (is.na(x['MAF'])){NA}
-  else if(as.numeric(x['MAF'])>=0.01){x['Variant_ID']} else{NA}})
+                                                                           else if(as.numeric(x['MAF'])>=0.01){x['Variant_ID']}
+                                                                           else{NA}})
 ## Order
 allD_vars_MAF_in_pops$Label <- factor(allD_vars_MAF_in_pops$Label, levels=c("2-234668879-C-CAT", "2-234668879-C-CATAT",
                                                                             "2-234638282-G-GT", "2-234622331-GC-G",
@@ -60,13 +93,6 @@ commonDvars_genes <- sapply(levels(allD_vars_MAF_in_pops$Label), function(x){as.
 commonDvars_genes[5] <- 'UGT1A[1-10]'
 commonDvars_genes[7] <- 'UGT2A1, UGT2A2'
 variant_labels_withGene <- paste0(levels(allD_vars_MAF_in_pops$Label), ' (', commonDvars_genes, ')')
-
-## Aggregated frequencies of D variants in each population
-aggregated_MAFs_per_pop <- melt(sapply(names(table(allD_vars_MAF_in_pops$Group)), function(x){sum(allD_vars_MAF_in_pops[which(!is.na(allD_vars_MAF_in_pops$MAF) & allD_vars_MAF_in_pops$Group==x), 'MAF'])}))
-aggregated_MAFs_per_pop$Group <- rownames(aggregated_MAFs_per_pop)
-colnames(aggregated_MAFs_per_pop)[1] <- 'aggregated_MAF'
-aggregated_MAFs_per_pop$aggregated_MAF <- signif(aggregated_MAFs_per_pop$aggregated_MAF, digits=3)
-aggregated_MAFs_per_pop$max_MAF <- sapply(names(table(allD_vars_MAF_in_pops$Group)), function(x){max(allD_vars_MAF_in_pops[which(!is.na(allD_vars_MAF_in_pops$MAF) &  allD_vars_MAF_in_pops$Group==x),'MAF'])})+0.01
 
 ## Different shapes for D variants with MAF>=0.01 in at least one population
 shapes <- c('2-234668879-C-CAT'=8,
@@ -82,6 +108,119 @@ shapes <- c('2-234668879-C-CAT'=8,
             '4-69811110-A-C'=4,
             '4-70078393-C-T'=0)
 
+# -------------------------------------------------------------------------------------
+#           Aggregated frequencies of deleterious variants in each population       
+# -------------------------------------------------------------------------------------
+
+aggs_pop <- list()
+aggs_just_per_gene <- list()
+
+for (group in names(table(allD_vars_MAF_in_pops$Group))){
+  
+  ## Accumulated frequency of all D UGT variants
+  allDvars_agg <- c('all_vars'=sum(subset(allD_vars_MAF_in_pops, !is.na(MAF) & Group==group)$MAF))
+  
+  ## Accumulated frequency of D UGT variants in each gene 
+  gene_aggs <- vector()
+  for (UGTgene in UGT_genes){
+    ## Gene variants
+    gene_vars <- subset(GMAFs_Dvars_genes, gene==UGTgene)$Variant_ID
+    gene_agg <- sum(subset(allD_vars_MAF_in_pops, !is.na(MAF) & Group==group & Variant_ID %in% gene_vars)$MAF)
+    gene_aggs[UGTgene] <- gene_agg
+  }
+
+  ## Accumulated frequency of D UGT variants in each gene family (UGT8 is the same as the gene)
+  gene_fam_aggs <- vector()
+  for(fam in gene_families[-4]){
+    ## Family variants
+    fam_vars <- unique(subset(GMAFs_Dvars_genes, gene_fam==fam)$Variant_ID)
+    fam_agg <- sum(subset(allD_vars_MAF_in_pops, !is.na(MAF) & Group==group & Variant_ID %in% fam_vars)$MAF)
+    gene_fam_aggs[fam] <- fam_agg
+  }
+  
+  ## Concatenate
+  aggs_pop[[group]] <- c(allDvars_agg, gene_fam_aggs, gene_aggs)
+  aggs_just_per_gene[[group]] <- gene_aggs
+
+}
+
+## All aggregated freqs
+aggs_pop <- as.data.frame(aggs_pop)
+aggs_pop$genetic_group <- rownames(aggs_pop)
+
+## Aggregated freqs of variants per individual gene
+aggs_just_per_gene <- as.data.frame(aggs_just_per_gene)
+aggs_just_per_gene$gene <- rownames(aggs_just_per_gene)
+aggs_just_per_gene$gene <- factor(aggs_just_per_gene$gene, levels=UGT_genes)
+aggs_just_per_gene <- melt(aggs_just_per_gene)
+aggs_just_per_gene$gene_fam <- substring(aggs_just_per_gene$gene, 1,4)
+
+## Stacked barplot 
+genes_colors <- c('UGT1A1'='thistle2',
+                  'UGT1A3'='plum2',
+                  'UGT1A4'='plum3',
+                  'UGT1A5'='pink',
+                  'UGT1A6'='lightpink2',
+                  'UGT1A7'='lightpink3',
+                  'UGT1A8'='hotpink1',
+                  'UGT1A9'='orchid3',
+                  'UGT1A10'='violetred',
+                  'UGT2A1'='peachpuff2',
+                  'UGT2A2'='sandybrown',
+                  'UGT2A3'='tan3',
+                  'UGT2B4'='lightsalmon1',
+                  'UGT2B7'='salmon',
+                  'UGT2B10'='lightsalmon3',
+                  'UGT2B11'='coral',
+                  'UGT2B15'='sienna3',
+                  'UGT2B17'='lightcoral',
+                  'UGT2B28'='chocolate4',
+                  'UGT3A1'='thistle3',
+                  'UGT3A2'='plum4',
+                  'UGT8'='lightskyblue3')
+
+ggplot(aggs_just_per_gene) +
+  geom_bar(aes(x = variable, y = value, fill = gene), 
+           position = "stack", stat = "identity") +
+  facet_wrap(~ gene_fam, scales="free_y", ncol=4) + 
+  ## Colors by 2nd variable
+  scale_fill_manual(values = genes_colors) +
+  theme_bw() +
+  labs(x='', y = 'Aggregated frequency of deleterious UGT variants per gene', fill='Gene') +
+  scale_x_discrete(breaks=c(paste0('MAF_',populations), 'Allele_Frequency'),
+                   labels=c("African/African American",
+                            "Latino/Admixed American",
+                            "Ashkenazi Jewish",
+                            "East Asian",
+                            "European Finnish",
+                            "European non Finnish",
+                            "South Asian",
+                            "Global")) +
+  theme(axis.title = element_text(size = (7), face='bold'),
+        axis.text = element_text(size = (6)),
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, face='bold'),
+        plot.subtitle = element_text(size = 7, color = "gray40"),
+        legend.title = element_text(size=9, face='bold'), 
+        legend.text = element_text(size=7.5, face='bold'), 
+        strip.background = element_rect(fill="gray95", size=1, color="gray60"),
+        strip.text = element_text(face="bold"))
+
+
+
+
+
+
+
+# -------------------------------------------------------------------------------------
+aggregated_MAFs_per_pop <- melt(sapply(names(table(allD_vars_MAF_in_pops$Group)), 
+                                       function(x){sum(allD_vars_MAF_in_pops[which(!is.na(allD_vars_MAF_in_pops$MAF) & allD_vars_MAF_in_pops$Group==x), 'MAF'])}))
+aggregated_MAFs_per_pop$Group <- rownames(aggregated_MAFs_per_pop)
+colnames(aggregated_MAFs_per_pop)[1] <- 'aggregated_MAF'
+aggregated_MAFs_per_pop$aggregated_MAF <- signif(aggregated_MAFs_per_pop$aggregated_MAF, digits=3)
+aggregated_MAFs_per_pop$max_MAF <- sapply(names(table(allD_vars_MAF_in_pops$Group)), 
+                                          function(x){max(allD_vars_MAF_in_pops[which(!is.na(allD_vars_MAF_in_pops$MAF) &  allD_vars_MAF_in_pops$Group==x),'MAF'])})+0.01
+
+## Plot
 ggplot(data = allD_vars_MAF_in_pops, mapping = aes(x = Group, y = MAF, color = Group)) +
   geom_point(data=subset(allD_vars_MAF_in_pops, is.na(Label)), alpha = 0.65, size = 1.3, 
              position = position_jitter(width = 0.1, height = 0), color="tomato") +
@@ -104,6 +243,7 @@ ggplot(data = allD_vars_MAF_in_pops, mapping = aes(x = Group, y = MAF, color = G
        subtitle=paste0(length(unique(allD_vars_MAF_in_pops$Variant_ID)), ' deleterious variants in total'), 
        x='', y='MAF of deleterious variants in each group', shape=paste0('Variant ID (MAF>0.01) & Gene(s)')) +
   coord_cartesian(ylim = c(0, max(subset(allD_vars_MAF_in_pops, !is.na(MAF))$MAF)), clip = 'off') +
+  ## Add aggregated frequency of all D variants per population
   geom_text(data=aggregated_MAFs_per_pop, aes(x=Group, y=max(subset(allD_vars_MAF_in_pops, !is.na(MAF))$MAF)+0.03, shape=NULL, label=aggregated_MAF), size=2.4, color='grey20', fontface='bold') +
   
   theme(plot.title = element_text(size = (9), face='bold', vjust = 7.1, hjust=0),
